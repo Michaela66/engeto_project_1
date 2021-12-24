4ab8540b5dad2698009bb3c9d3ee327cd3fb06ba
Michaela66 <96316630+Michaela66@users.noreply.github.com>
2021-12-24 16:23:46 +0100

    Add files via upload

--- text_analyzer.py
+++ text_analyzer.py
@@ -40,9 +40,12 @@ garpike and stingray are also present."""
 username = input('username:')
 password = input('password:')
 
-if registrovani.get(username) != password:
-    print('Heslo, nebo uživatelské jméno je špatně!')
-    quit()
+if username not in registrovani:
+     print('unregistered user, terminating the program..')
+     quit()
+elif username in registrovani and registrovani.get(username) != password:
+     print('wrong password, terminating the program..')
+     quit()
 else:
      print(f"""{oddelovac}
 Welcome to the app, {username}
@@ -71,15 +74,50 @@ for word in vycisteny_text_1:
     if word.isupper() and word.isalpha():
         uppercase_1 += 1
 
-vyber_cisla = input('Enter a number btw. 1 and 3 to select:')
-if vyber_cisla.isnumeric() == True and  vyber_cisla not in TEXTS.keys():
-    print ('Toto číslo není v rozmezí 1-3')
-    quit()
-elif vyber_cisla.isnumeric() == False:
-    print ('Zadaná hodnota není číslo')
-    quit()
-elif vyber_cisla == '1':
-    print(oddelovac)
-    print (f"There are {celkem_1} words in the selected text.")
-    print (f"There are {title_1} titlecase words.")
-    print(f"There are {uppercase_1} uppercase words.")
+# lowercase words pro text 1
+lowercase_1 = 0
+for word in vycisteny_text_1:
+    if word.islower() and word.isalpha():
+        lowercase_1 += 1
+
+# numeric strings
+numeric_1 = 0
+for word in vycisteny_text_1:
+    if word.isnumeric():
+        numeric_1 += 1
+
+# all numbers
+all_numbers_1 = []
+for number in vycisteny_text_1:
+    if number.isnumeric():
+        all_numbers_1.append(int(number))
+sum_numbers_1 = sum(all_numbers_1)
+
+# delka slov
+delka_slov_1= dict()
+for slovo in vycisteny_text_1:
+        delka_slov_1[slovo] = len(slovo)
+
+print(delka_slov_1)
+# poradi
+
+
+#
+# vyber_cisla = input('Enter a number btw. 1 and 3 to select:')
+# if vyber_cisla.isnumeric() == True and  vyber_cisla not in TEXTS.keys():
+#     print ('Toto číslo není v rozmezí 1-3')
+#     quit()
+# elif vyber_cisla.isnumeric() == False:
+#     print ('Zadaná hodnota není číslo')
+#     quit()
+# elif vyber_cisla == '1':
+#     print(oddelovac)
+#     print (f"There are {celkem_1} words in the selected text.")
+#     print (f"There are {title_1} titlecase words.")
+#     print (f"There are {uppercase_1} uppercase words.")
+#     print (f"There are {lowercase_1} lowercase words.")
+#     print (f"There are {numeric_1} numeric strings.")
+#     print (f"The sum of all the numbers {sum_numbers_1}.")
+#     print (oddelovac)
+#     print (f" LEN| OCCURENCES| NR.")
+#     print(oddelovac)
